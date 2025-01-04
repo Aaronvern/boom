333e5e0cc89ddc6f2b3539d1dde9e23538a90999
Aaronvern <aaronvernekar@gmail.com>
2025-01-04 23:13:53 +0530

    finally working fine

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -9,8 +9,10 @@ const HomeLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
       <div className='flex'>
         <Sidebar/>
         <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
-          <div className='w-full'>{children}</div> 
-
+          <div className='w-full'>{children}</div>
+          <div className='fixed bottom-0 right-0 mr-6 mb-6'>
+            <h2 className='text-1xl text-slate-400 font-mono'>built by aaronvern</h2> 
+          </div>
         </section>
       </div>
     </main>
--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -61,8 +61,8 @@ const MeetingRoom = () => {
           <CallParticipantsList onClose={() => setShowParticipants(false)} />
         </div>
       </div>
-      {/* video layout and call controls */}//sddfgdfgdsdbsdkhb
-      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+      {/* video layout and call controls */}
+      <div className="fixed bottom-0 flex flex-wrap w-full items-center justify-center gap-5 py-3 ">
         <CallControls onLeave={() => router.push(`/`)} />
 
         <DropdownMenu>
